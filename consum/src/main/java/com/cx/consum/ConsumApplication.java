f751e1771e867582d14c2bb0aaa4c68b9e756d7c
caixin <836566274@qq.com>
2020-03-10 17:14:21 +0800

    打开断路器 Hystrix

--- consum/src/main/java/com/cx/consum/ConsumApplication.java
+++ consum/src/main/java/com/cx/consum/ConsumApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author cx
@@ -20,6 +23,12 @@ public class ConsumApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
--- consum/src/main/java/com/cx/consum/moudle/hello/controller/HelloController.java
+++ consum/src/main/java/com/cx/consum/moudle/hello/controller/HelloController.java
@@ -2,13 +2,11 @@ package com.cx.consum.moudle.hello.controller;
 
 import com.cx.consum.moudle.hello.service.HelloService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @Description TODO
